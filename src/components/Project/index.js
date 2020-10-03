9da8f8492dbb4ad5d6d8ff894d2e10fd9f081b07
Jason Ahmed <jason_ahmed@yahoo.com>
2020-10-03 16:37:36 -0700

    set-up gallery

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import photo from "../../assets/small/commercial/book-garden-screenshot-small.png";
 import photo2 from "../../assets/small/commercial/portfolio-screenshot-small.png";
 import photo3 from "../../assets/small/commercial/run-buddy-screenshot-small.png";
+import photo4 from "../../assets/small/commercial/taskinator-screenshot-small.png";
+import photo5 from "../../assets/small/commercial/weather-dashboard-screenshot-small.png";
+import photo6 from "../../assets/small/commercial/workday-scheduler-screenshot-small.png";
 
 
 function Project() {
@@ -11,17 +14,24 @@ function Project() {
         <p>GALLERY</p>
       <div className="flex-row">
           <img
-            src={photo} alt="Book Garden screenshot" className="img-thumbnail mx-1"
+            src={photo} alt="Book Garden screenshot" className="img-thumbnail mx-2"
+          />
+          <img
+            src={photo2} alt="Portfolio screenshot" className="img-thumbnail mx-2"
+          />
+          <img
+            src={photo3} alt="Run Buddy screenshot" className="img-thumbnail mx-2"
           />
       </div>
       <div className="flex-row">
           <img
-            src={photo2} alt="Portfolio screenshot" className="img-thumbnail mx-1"
+            src={photo4} alt="Taskinator screenshot" className="img-thumbnail mx-2"
           />
-      </div>
-      <div className="flex-row">
           <img
-            src={photo3} alt="Run Buddy screenshot" className="img-thumbnail mx-1"
+            src={photo5} alt="Weather Dashboard screenshot" className="img-thumbnail mx-2"
+          />
+          <img
+            src={photo6} alt="Workday Scheduler screenshot" className="img-thumbnail mx-2"
           />
       </div>
       </section>
