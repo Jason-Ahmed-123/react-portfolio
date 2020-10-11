5c9844f555d8d416df19b1870957f454ccd3655c
Jason Ahmed <jason_ahmed@yahoo.com>
2020-10-11 10:01:13 -0700

    rearrange Gallery for smaller screens

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -12,21 +12,23 @@ function Project() {
       <section>
         <h1 id="project">Gallery</h1>
         <p>A sampling of my projects</p>
-      <div className="flex-row imageHolder">
+      <div className="flex-row">
       <a href="https://jason-ahmed-123.github.io/groupproject1-book-garden/" target = "">
           <img src={photo} alt="Book Garden screenshot" className="img-thumbnail mx-2"/>
       </a>
       <a href = "https://jason-ahmed-123.github.io/run-buddy/" target = "">
           <img src={photo3} alt="Run Buddy screenshot" className="img-thumbnail mx-2"/>
       </a>
+      </div>
+      <div className="flex-row">
       <a href="https://jason-ahmed-123.github.io/taskinator/" target = "">
           <img src={photo4} alt="Taskinator screenshot" className="img-thumbnail mx-2"/>
       </a>
-      </div>
-      <div className="flex-row">
       <a href="https://jason-ahmed-123.github.io/Week2-Challenge-Portfolio/" target = "">
           <img src={photo2} alt="Portfolio screenshot" className="img-thumbnail mx-2"/>
       </a>
+      </div>
+      <div className="flex-row">
       <a href="https://jason-ahmed-123.github.io/week6-weather-dashboard/" target = "">
           <img src={photo5} alt="Weather Dashboard screenshot" className="img-thumbnail mx-2"/>
       </a>
